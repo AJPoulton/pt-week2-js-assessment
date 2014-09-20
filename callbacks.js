c3b483894a4eae37e2776c6a4721f6d2954105c9
Aaron Poulton <aaronpoulton@gmail.com>
2014-09-20 13:07:27 -0600

    i thought i had it until this assessment

--- callbacks.js
+++ callbacks.js
@@ -1,11 +1,12 @@
 //Make the following function calls work
-  giveItName('name', function(){
+  giveItName(aaron, function(){
     alert('Your name is ' + name);
   });
 
   var nums = [2,4,5,6];
   //returns true if all items in the array are evens.
   areEvens(nums, function(item){
+    if(nums[.length] === true);
     return item % 2 === 0; 
   });
 
