09e0addc6a8bd50f62bf60cd6678c9f12358ff84
Aaron Poulton <aaronpoulton@gmail.com>
2014-09-20 13:01:54 -0600

    best i could remember

--- arrays.js
+++ arrays.js
@@ -1,34 +1,42 @@
 //create an array called family which has your family members in order of their age
 
+
   //code here
 
+ var family = ['Aaron', 'Brittany', 'Tilly', 'Maddox', 'Travis', 'Crue']
 
 
 //Remove the oldest member of your family
 
   //code here
-
+family.shift[0];
 
 
 //Add a new family member as the youngest member
 
   //code here
-
+family.push('newBaby');
 
 //LOOP over your array to see if you have a family member named 'Tyler'
 // If you do, alert ('I have a Tyler'), if you do not, alert ('I have no Tylers')
 
   //Code here
-
+for(see 'Tyler' family)??
 
 // Remove the second oldest member of your family
 
   //Code here
-
+family.slice[1];??
 
 
 //Write a function called 'reverse' that takes in a string and returns that string in reverse order
 
   //Function definition here
-
+var reverse = function(x){
+  var x = family;
+  return x.reverse(x.length);
+}
   //Call the function here
+reverse(family)
+
+
